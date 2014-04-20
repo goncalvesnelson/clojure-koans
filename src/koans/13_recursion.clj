2bdc22f51a612c57703b9276cdafa2afc8c75a0b
goncalvesnelson <goncalvesnelson@gmail.com>
2014-04-20 01:29:09 +0100

    Exercises up to 14

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -1,20 +1,30 @@
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+   (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [coll coll
+        coll_acc '()]
+  (if (empty? coll)
+    coll_acc
+    (recur (rest coll) (cons (first coll) coll_acc))
+)))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+        acc 1]
+    (if (zero? n)
+      acc
+      (recur (dec n) (* n acc)))
+))
 
 (meditations
   "Recursion ends with a base case"
